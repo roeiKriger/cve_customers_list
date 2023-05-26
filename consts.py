d94913918a2a2d87202182bc22032a610fdd41d9
Roei Kriger <roeiKriger1@gmail.com>
2023-05-26 11:19:31 +0300

    Changing the logic of the program from scraping to API useage

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 # The interesting url
-cve_url_list = "https://nvd.nist.gov/vuln/full-listing/2023/5"
-site_base_url = "https://nvd.nist.gov"
+#cve_url_list = "https://nvd.nist.gov/vuln/full-listing/2023/5"
+#site_base_url = "https://nvd.nist.gov"
 number_of_links_to_scan = 50
 # At the moment the url length is 45, but there are months that are also more than one digit, so added 1 to 46
 # #(in the future will just check the month with DATE TIME and then will validate)
--- main.py
+++ main.py
@@ -8,23 +8,44 @@ customers_list = read_customers_to_an_array()
 for customer in customers_list:
     print(customer)
 
-check_url_integrity(cve_url_list, url_len, regex_rule_for_url)
-links_of_the_month = get_cve_monthly_links()
+
+url = 'https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=Microsoft&pubStartDate=2023-03-04T00:00:00.000&pubEndDate=2023-05-05T00:00:00.000&cvssV3Severity=CRITICAL&resultsPerPage=100'
+base_url = 'https://services.nvd.nist.gov/rest/json/cves/2.0?'
+keyword_customer_name = 'keywordSearch='
+customer_name = 'Microsoft'
+dates = 'pubStartDate=2023-03-04T00:00:00.000&pubEndDate=2023-05-05T00:00:00.000'
+cvss_severity = 'cvssV3Severity=CRITICAL'
+max_results_to_return = 'resultsPerPage=100'
+
+full_url = base_url+keyword_customer_name+customer_name+'&'+dates+'&'+cvss_severity+'&'+max_results_to_return
+
+response = requests.get(full_url)
+page_content = response.text
+print(page_content)
 
 
-# Extract the text inside the <p> tag from each link
-data = []
-for link in links_of_the_month:
-    response = requests.get(link)
-    page_content = response.text
-    soup = BeautifulSoup(page_content, "html.parser")
-    paragraph = soup.find("p", attrs={"data-testid": "vuln-description"})
-    if paragraph:
-        data.append(paragraph.text)
-    break
 
-# Print the extracted text
-for item in data:
-    print(item)
-    break
+
+
+
+
+# check_url_integrity(cve_url_list, url_len, regex_rule_for_url)
+# links_of_the_month = get_cve_monthly_links()
+#
+#
+# # Extract the text inside the <p> tag from each link
+# data = []
+# for link in links_of_the_month:
+#     response = requests.get(link)
+#     page_content = response.text
+#     soup = BeautifulSoup(page_content, "html.parser")
+#     paragraph = soup.find("p", attrs={"data-testid": "vuln-description"})
+#     if paragraph:
+#         data.append(paragraph.text)
+#     break
+#
+# # Print the extracted text
+# for item in data:
+#     print(item)
+#     break
 
